927e3cc21aa473de515a8ea0cbcef697f4ae2b92
Jinyi Liu <liujy0129@icloud.com>
2020-10-21 00:41:58 +0800

    daka

---
+++ bnu_daka_mini.py
@@ -0,0 +1,138 @@
+import os
+import requests, json, re
+import time, datetime
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+class DaKa(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.info = None
+
+        self.login_url = "https://onewechat.bnu.edu.cn/uc/wap/login?redirect=https%3A%2F%2Fonewechat.bnu.edu.cn%2Fncov%2Fwap%2Fdefault%2Findex"
+        self.base_url = "https://onewechat.bnu.edu.cn/ncov/wap/default/index"
+        self.save_url = "https://onewechat.bnu.edu.cn/ncov/wap/default/save"
+        self.login_check_url = "https://onewechat.bnu.edu.cn/uc/wap/login/check"
+        self.sess = requests.Session()
+
+
+    def login(self):
+        """Login to BNU platform"""
+        res = self.sess.get(self.login_url)
+        
+        if res.status_code != 200:
+            raise Exception('{} login failed, status code {}'.format(self.username,res.status_code))
+
+        data = {'username': self.username,
+                'password': self.password,}
+        
+        res = self.sess.post(url=self.login_check_url, data=data)
+        ret = json.loads(res.content.decode())
+
+        if ret['e'] != 0:
+            raise Exception("{} login failed, reason {}".format(self.username,ret['m']))
+        return ret
+    
+    def post(self):
+        """Post the hitcard info"""
+        res = self.sess.post(self.save_url, data=self.info)
+        if res.status_code != 200:
+            raise Exception("{} post info faild, status code {}".format(self.username, res.status_code))
+        return json.loads(res.text)
+
+    @ staticmethod
+    def get_date():
+        today = datetime.date.today()
+        return "%4d%02d%02d" % (today.year, today.month, today.day)
+        
+    def get_info(self, html=None):
+        """Get hitcard info, which is the old info with updated new time."""
+        if not html:
+            res = self.sess.get(self.base_url)
+            if res.status_code != 200:
+                raise Exception("{} get info faild, statu code {}".format(self.username, res.status_code))
+            html = res.content.decode()
+        old_info = json.loads(re.findall(r'oldInfo: (.*)', html)[0][:-1])
+        print("old_info:", old_info)
+        name = re.findall(r'realname: "([^\"]+)",', html)[0]
+        number = re.findall(r"number: '([^\']+)',", html)[0]
+
+        new_info = old_info.copy()
+        new_info['name'] = name
+        new_info['number'] = number
+        new_info["date"] = self.get_date()
+        new_info["created"] = round(time.time())
+
+        self.info = new_info
+        self.save_info()
+
+        return new_info
+
+    @ staticmethod
+    def _rsa_encrypt(self, password_str, e_str, m_str):
+        password_bytes = bytes(password_str, 'ascii') 
+        password_int = int.from_bytes(password_bytes, 'big')
+        e_int = int(e_str, 16) 
+        m_int = int(m_str, 16)
+        result_int = pow(password_int, e_int, m_int)
+        return hex(result_int)[2:].rjust(128, '0')
+
+    def save_info(self):
+        with open("cache_info_bk.json", "w", encoding="utf-8") as file:
+            json.dump(self.info, file)
+
+
+    def daka(self):
+        ret = []
+        res = self.login()
+        ret.append(res)
+        res = self.get_info()
+        ret.append(res)
+        res = self.post()
+        ret.append(res)
+        ret.append(res)
+        return ret
+
+
+def main(username, password):
+
+    dk = DaKa(username, password)
+    try:
+        dk.daka()
+    except Exception as e:
+        print(e)
+    finally:
+        return 0
+
+
+def run():
+    if not os.path.exists('./config.json'):
+        print('Please create file config.json to {}.'.format(os.getcwd()))
+        return
+
+    configs = json.loads(open('./config.json', 'r').read())
+    for config in configs["info"]:
+        username = config["username"]
+        password = config["password"]
+        scheduler_flag = config["schedule"]["on"]
+        hour = config["schedule"]["hour"]
+        minute = config["schedule"]["minute"]
+
+        if scheduler_flag:
+            # Schedule task
+            scheduler = BlockingScheduler()
+            scheduler.add_job(main, 'cron', args=[username, password], hour=hour, minute=minute)
+            print('Check in for you at %02d:%02d everyday.' % (int(hour), int(minute)))
+            print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+            try:
+                scheduler.start()
+            except (KeyboardInterrupt, SystemExit):
+                pass
+        else:
+            main(username, password)
+
+
+if __name__ == "__main__":
+    run()
